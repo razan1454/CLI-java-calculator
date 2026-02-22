5d7cebc2e752b7b0535c01b9e26221b54c6e25f1
KhadijaAli <alloughanikhadijaali@gmail.com>
2026-02-22 22:42:30 +0400

    4- Modulus & Absolute Value

--- src/MAIN.java
+++ src/MAIN.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class main{
-    public static void main(String [] args) {
+public class main {
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter first number : ");
@@ -13,22 +13,22 @@ public class main{
         System.out.println("--\sTRIANGLE PATTERN\s--");
         System.out.println("Enter number of Rows: ");
 
-        if(scanner.hasNextInt()){
+        if (scanner.hasNextInt()) {
             int row = scanner.nextInt();
-            if (row>0){
-                for(int x = 1;x<=row;x++){
-                    for(int y =1;y<=(row - x);y++){
+            if (row > 0) {
+                for (int x = 1; x <= row; x++) {
+                    for (int y = 1; y <= (row - x); y++) {
                         System.out.print(" ");
                     }
-                    for(int z=1; z<=x;z++){
+                    for (int z = 1; z <= x; z++) {
                         System.out.print("* ");
                     }
                     System.out.println();
                 }
-            }else{
+            } else {
                 System.out.println("Error: Number of rows must be a positive int!");
             }
-        }else{
+        } else {
             System.out.println("Error: Invalid INP!");
             scanner.next();
         }
@@ -38,26 +38,24 @@ public class main{
         System.out.println("---\sINVERTED TRIANGLE PATTERN\s---");
         System.out.println("Enter number of Rows: ");
 
-        if(scanner.hasNextInt()){
-            int row= scanner.nextInt();
-            if(row>0){
-                for(int b=row;b>=1; b--){
-                    for(int c =1;c<=(row-b);c++){
+        if (scanner.hasNextInt()) {
+            int row = scanner.nextInt();
+            if (row > 0) {
+                for (int b = row; b >= 1; b--) {
+                    for (int c = 1; c <= (row - b); c++) {
                         System.out.print(" ");
                     }
-                    for (int j =1; j<=b;j++){
+                    for (int j = 1; j <= b; j++) {
                         System.out.print("* ");
                     }
                     System.out.println();
                 }
-            }else{
+            } else {
                 System.out.println("Error: Number of rows must be a positive number");
             }
-        }else{
+        } else {
             System.out.println("INvalid Input!");
         }
-
-        scanner.close();
 
 
         //3 maryam
@@ -65,10 +63,9 @@ public class main{
         int product = num1 * num2;
         System.out.println(num1 + "*" + num2 + "=" + product);
 
-        if (num2 == 0 ){
+        if (num2 == 0) {
             System.out.println("❌ Error: Cannot divide by zero");
-        }
-        else {
+        } else {
             System.out.println("--- DIVISION ---");
             int quotient = num1 / num2;
             System.out.println(num1 + "÷" + num2 + "=" + quotient);
@@ -79,32 +76,34 @@ public class main{
         System.out.println("--- FACTORIAL ---");
         System.out.println("Enter a non-negative integer: ");
         num1 = scanner.nextInt();
-        if (num1 >= 1){
+        if (num1 >= 1) {
             int factorial = 1;
             for (int i = num1; i >= 1; i--) {
                 factorial = factorial * i;
             }
-            System.out.println(num1+"! = " + factorial);
-        }else if(num1 == 0){
+            System.out.println(num1 + "! = " + factorial);
+        } else if (num1 == 0) {
             System.out.println("0! = 1");
-        }else {System.out.println("Error: Factorial is not defined for negative numbers.");}
+        } else {
+            System.out.println("Error: Factorial is not defined for negative numbers.");
+        }
 
 
         System.out.println("--- EVEN / ODD CHECK ---");
         System.out.println("Enter a number: ");
         num1 = scanner.nextInt();
         if (num1 % 2 == 0) {
-            System.out.println(num1+" is Even.");
+            System.out.println(num1 + " is Even.");
         }
-        System.out.println(num1+ " is Odd.");
+        System.out.println(num1 + " is Odd.");
 
         System.out.println("--- ADDITION ---");
         System.out.print(" Enter first number: ");
         int firstNumber = scanner.nextInt();
         System.out.print(" Enter second number: ");
         int secondNumber = scanner.nextInt();
-        int additionResult=firstNumber+secondNumber;
-        System.out.printf(" %d + %d = %d %n",firstNumber,secondNumber, additionResult);
+        int additionResult = firstNumber + secondNumber;
+        System.out.printf(" %d + %d = %d %n", firstNumber, secondNumber, additionResult);
 
         // 4- Division
         System.out.println("Enter The dividend number:  ");
@@ -115,22 +114,21 @@ public class main{
         System.out.printf("%d  OVER  %d  = %.2f", dividendNum, divisorNum, resultOfDiv);
 
 
-
         System.out.println("--- SUBTRACTION ---");
         System.out.print(" Enter first number: ");
         int subtractFirstNumber = scanner.nextInt();
         System.out.print(" Enter second number: ");
         int subtractSecondNumber = scanner.nextInt();
-        int subtractionResult=subtractFirstNumber-subtractSecondNumber;
-        System.out.printf(" %d - %d = %d%n",subtractFirstNumber,subtractSecondNumber,subtractionResult);
+        int subtractionResult = subtractFirstNumber - subtractSecondNumber;
+        System.out.printf(" %d - %d = %d%n", subtractFirstNumber, subtractSecondNumber, subtractionResult);
 
         System.out.println("--- SUBTRACTION ---");
         System.out.print(" Enter first number: ");
         int reSubtractFirstNumber = scanner.nextInt();
         System.out.print(" Enter second number: ");
         int reSubtractSecondNumber = scanner.nextInt();
-        int reSubtractionResult=reSubtractFirstNumber-reSubtractSecondNumber;
-        System.out.printf(" %d - %d = %d",reSubtractFirstNumber,reSubtractSecondNumber,reSubtractionResult);
+        int reSubtractionResult = reSubtractFirstNumber - reSubtractSecondNumber;
+        System.out.printf(" %d - %d = %d", reSubtractFirstNumber, reSubtractSecondNumber, reSubtractionResult);
 
 
         //power
@@ -143,7 +141,7 @@ public class main{
 
         int result = 1;
 
-        for (int i = 0; i < exponent; i++){
+        for (int i = 0; i < exponent; i++) {
             result *= base;
         }
         System.out.println(base + " ^ " + exponent + " = " + result);
@@ -155,9 +153,9 @@ public class main{
 
         if (number1 < 0) {
             System.out.println("Error: Cannot compute square root of a negative number.");
-        }else {
+        } else {
             double sqrt = Math.sqrt(number1);
-            System.out.println(" √ "+ number1 + " = " + sqrt);
+            System.out.println(" √ " + number1 + " = " + sqrt);
         }
 
 // 77 Reverse Number
@@ -182,41 +180,38 @@ public class main{
             System.out.println(original + " is NOT a Palindrome");
         }
 
+        //10:
+        System.out.println("--- NUMBER STAIRCASE PATTERN ---");
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+        if (rows <= 0) {
+            System.out.println(" Error: Number of rows must be positive.");
+            return;
+        }
+        for (int i = 1; i <= rows; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        //Multiplication
+
+        System.out.println("--- MULTIPLICATION TABLE ---");
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+
+        }
+
+
+        scanner.close();
 
 
     }
 
-
 }
 
-//10:
-
-static void numberStaircase() {
-Scanner sc = new Scanner (System.in);
-    System.out.println("--- NUMBER STAIRCASE PATTERN ---");
-    System.out.print("Enter number of rows: ");
-    int rows = sc.nextInt();
-    if (rows <= 0) {
-   System.out.println(" Error: Number of rows must be positive.");
-   return;
-    }
- for (int i = 1; i <= rows; i++) {
-
-  for (int j = 1; j <= i; j++) {
-  System.out.print(j + " ");
-}
-  System.out.println();
-    }
-}
-//Multiplication
-static void multiplicationTable() {
-
- System.out.println("--- MULTIPLICATION TABLE ---");
- System.out.print("Enter a number: ");
-    int n = sc.nextInt();
-    for (int i = 1; i <= 10; i++) {
- System.out.println(n+ " x " + i + " = " + (n* i));
-
-    }
-}
 
