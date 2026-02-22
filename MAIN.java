2ad520f284c47fa39a5616738290359da74fb5d0
Razansalam <rooozalharthi@gmail.com>
2026-02-22 11:19:02 +0400

    menu add

--- MAIN.java
+++ MAIN.java
@@ -2,13 +2,88 @@ import java.util.Scanner;
 
 public class MAIN{
     public static void main(String [] args) {
-
+         System.out.println("CLI Java Calculator");
+        System.out.println("*********************");
+        System.out.println("1.Addition. \n 2.Subtraction .\n 3. Multiplication .\n 4. Division.\n  5.Modulus " +
+                "\n 6.Absolute Value \n 7.power. \n 8.Square Root .\n  9. Factorial.\n 0.Exit .\n 10. Even / Odd Check. \n 11.  Reverse a Number. \n 12.Palindrome Check.\n 13.Sum of Digits. \n 14. Count Digits \n 15. Triangle Pattern. \n 16.Inverted Triangle Pattern.\n 17. Number Staircase Pattern. \n 18.Multiplication Table ");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("enter first number : ");
-        int num1 = scanner.nextInt();
-        System.out.println("enter second number : ");
-        int numb2 = scanner.nextInt();
+        System.out.println("Enter your choice : ");
+        int choice = scanner.nextInt();
 
+        if (choice == 1 ){
+            //addition
+        }
+        if (choice == 2 ){
+            //sub
 
+        }
+        if (choice == 3 ){
+            //multi
+
+        }
+        if (choice == 4 ){
+            //divi
+
+        }
+        if (choice == 5 ){
+            // moduls
+
+        }
+        if (choice == 6 ){
+            //absolute
+
+        }
+        if (choice == 7 ){
+            //power
+
+        }
+        if (choice == 8 ){
+            //square
+
+        }
+        if (choice == 9 ){
+            //exit
+
+        }
+        if (choice == 0 ){
+            //exit
+
+        }
+        if (choice == 10 ){
+            //even / odd
+
+        }
+        if (choice == 11 ){
+            //reverse
+
+        }
+        if (choice == 12 ){
+            //palindrome
+
+        }
+        if (choice == 13 ){
+            //sum
+
+        }
+        if (choice == 14 ){
+            //count
+
+        }
+        if (choice == 15 ){
+            //Triangle
+
+        }
+        if (choice == 16 ){
+            //inverted
+
+        }
+        if (choice == 17 ){
+            //number
+
+        }
+        if (choice == 18 ){
+            // Multiplication Table
+
+        }
     }
 }
