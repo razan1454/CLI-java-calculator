8d4684bbcd8abfbc781a40d086c4379781a4d090
Razansalam <rooozalharthi@gmail.com>
2026-02-22 10:22:25 +0400

    first push

---
+++ MAIN.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MAIN{
+    public static void main(String [] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter first number : ");
+        int num1 = scanner.nextInt();
+        System.out.println("enter second number : ");
+        int numb2 = scanner.nextInt();
+
+
+    }
+}
